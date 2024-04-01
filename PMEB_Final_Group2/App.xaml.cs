3759fc5402759afa0a4db36b38bdf824d1e8d06a
Ian Christian <w0480449@nscc.ca>
2024-04-01 20:41:12 -0300

    Setup basic WPF project in repo. Ran scaffold command and modified context class for safe connection string

---
+++ PMEB_Final_Group2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PMEB_Final_Group2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
